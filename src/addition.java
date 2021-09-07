e27375d0d8fa148cf654f43c0dfcac2dcb9cc658
Meghana <meghana1154k@gmail.com>
2021-09-07 10:31:06 +0200

    Initial commit

---
+++ src/addition.java
@@ -0,0 +1,40 @@
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+
+public class addition {
+
+    public static void main(String[] args){
+
+    }
+
+
+
+
+    public int add(int a, int b){
+        int c = a+b;
+        System.out.println("Sum " + c);
+        return c;
+    }
+
+    @Ignore("")
+    @Test
+    public void Test1()
+    {
+        addition a = new addition();
+        int c = a.add(5,6);
+
+
+        Assert.assertEquals(11, c);
+    }
+
+    @Test
+    public void Test2()
+    {
+        addition a = new addition();
+        int c = a.add(5,6);
+
+        Assert.assertEquals(10, c);
+    }
+}
